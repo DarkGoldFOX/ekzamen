f628a657cd8bb9e2f57ea37d8feaa9359447ef8f
DarkGoldFOX <90600446+DarkGoldFOX@users.noreply.github.com>
2022-02-25 11:10:28 +0300

    init

---
+++ Desktop/WpfApp1/WpfApp1/Services.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Services.xaml
+    /// </summary>
+    public partial class Services : Page
+    {
+        public Services()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Admin.MainFrame.Navigate(new AddEdit());
+        }
+
+    }
+}
